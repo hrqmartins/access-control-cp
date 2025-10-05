a5104b947269a2c37d76e3b473f84ea84a8ccb79
HenriqueTcesar <rickcesar1717@gmail.com>
2025-10-05 18:41:39 -0300

    Estilizando totalmente a página de login

--- access-control-cp/src/routes/Login/index.tsx
+++ access-control-cp/src/routes/Login/index.tsx
@@ -17,20 +17,22 @@ const LoginPage: React.FC = () => {
   };
 
   return (
-    <div>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <form
-        onSubmit={handleSubmit}>
-        <h2>Login</h2>
+        onSubmit={handleSubmit}
+        className="bg-white shadow-lg rounded-xl p-8 w-full max-w-md"
+      >
+        <h2 className="text-2xl font-bold text-center mb-6">Login</h2>
 
-        <div>
-          <label>Nome</label>
+        <div className="mb-4">
+          <label className="block text-gray-700 mb-2">Nome</label>
           <input type="text" value={nomeUsuario} onChange={(e) => setNomeUsuario(e.target.value)} className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring focus:border-blue-300" placeholder="Digite seu nome" required/>
         </div>
-        <div>
-          <label>Email</label>
-          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Digite seu email" required/>
+        <div className="mb-6">
+          <label className="block text-gray-700 mb-2">Email</label>
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring focus:border-blue-300" placeholder="Digite seu email" required/>
         </div>
-        <button type="submit">
+        <button type="submit" className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition">
           Entrar
         </button>
       </form>
