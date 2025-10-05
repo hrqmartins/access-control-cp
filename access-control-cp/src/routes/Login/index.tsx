a92e948434229b63a746424bfabdd94057640ba4
HenriqueTcesar <rickcesar1717@gmail.com>
2025-10-05 18:41:12 -0300

    Criando a página de login, com uState, useNavigate, useContext e o form para o nome e email.

--- access-control-cp/src/routes/Login/index.tsx
+++ access-control-cp/src/routes/Login/index.tsx
@@ -1,10 +1,41 @@
-import { Link } from 'react-router-dom';
+import React, { useState, useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
-export function Login() {
+const LoginPage: React.FC = () => {
+  const { login } = useContext(AuthContext);
+  const [nomeUsuario, setNomeUsuario] = useState("");
+  const [email, setEmail] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    login({ nome: nomeUsuario, email });
+
+    navigate("/");
+  };
+
   return (
     <div>
-      <h1>Página de Login</h1>
-      <p>Não tem uma conta? <Link to="../Cadastro">Cadastre-se</Link></p>
+      <form
+        onSubmit={handleSubmit}>
+        <h2>Login</h2>
+
+        <div>
+          <label>Nome</label>
+          <input type="text" value={nomeUsuario} onChange={(e) => setNomeUsuario(e.target.value)} className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring focus:border-blue-300" placeholder="Digite seu nome" required/>
+        </div>
+        <div>
+          <label>Email</label>
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Digite seu email" required/>
+        </div>
+        <button type="submit">
+          Entrar
+        </button>
+      </form>
     </div>
   );
-}
+};
+
+export default LoginPage;
