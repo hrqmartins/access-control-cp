6af42bd179dbdaf6df3d87f7c8a951b42875346e
HenriqueTcesar <rickcesar1717@gmail.com>
2025-10-05 18:29:13 -0300

    Criando e estilizando o componente Rodape

--- access-control-cp/src/components/Rodape/Rodape.tsx
+++ access-control-cp/src/components/Rodape/Rodape.tsx
@@ -1,8 +1,13 @@
-export default function Rodape(){
+import React from "react";
 
-    return(
-        <footer>
-            <p>&copy; Todos os direitos reservados - 2025. App</p>
-        </footer>
-    );
-}
+const Rodape: React.FC = () => {
+  return (
+    <footer className="bg-gray-900 text-white py-4 text-center">
+      <div className="text-sm">
+        Projeto Access Control CP — FIAP · Entrega CP5
+      </div>
+    </footer>
+  );
+};
+
+export default Rodape;
