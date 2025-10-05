c061e9ca8b9f73ecef2b4c417dc4eb27fa059f30
HenriqueTcesar <rickcesar1717@gmail.com>
2025-10-05 18:23:23 -0300

    Adicionando as rotas no app e os caminhos para cada páginas

--- access-control-cp/src/App.tsx
+++ access-control-cp/src/App.tsx
@@ -1,8 +1,34 @@
-function App() {
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Cabecalho from "./components/Cabecalho/Cabecalho";
+import Rodape from "./components/Rodape/Rodape";
+import Login from "./routes/Login";
+import Cadastro from "./routes/Cadastro";
+import Home from "./routes/Home";
+import ErrorPage from "./routes/Error";
+import { AuthProvider } from "./context/AuthContext";
+import RotaPV from "./context/RotaPV";
+
+const App: React.FC = () => {
   return (
-    <>
-    <h1>Ola Mundo!</h1>
-    </>
+    <AuthProvider>
+      <BrowserRouter>
+        <div className="min-h-screen flex flex-col">
+          <Cabecalho />
+          <main className="flex-1 container mx-auto px-4 py-8">
+            <Routes>
+              <Route path="/" element={<RotaPV><Home /></RotaPV>} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/404" element={<ErrorPage />} />
+              <Route path="*" element={<Navigate to="/404" replace />} />
+            </Routes>
+          </main>
+          <Rodape />
+        </div>
+      </BrowserRouter>
+    </AuthProvider>
   );
-}
+};
+
 export default App;
